fe34ff01556fe6f62efaa43cee31db46cc70112b
Cristian Andres Arenas Vargas <crarenasv@unal.edu.co>
2026-06-28 20:26:34 -0500

    Contacto actualizado2

--- src/components/pages.js
+++ src/components/pages.js
@@ -171,12 +171,12 @@ export function buildContactPage() {
         <div class="contact-item">
           <div class="contact-icon">✉</div>
           <div class="contact-label">Email</div>
-          <div class="contact-value">hola@alejandromora.dev</div>
+          <div class="contact-value">crarenasv@unal.edu.co</div>
         </div>
         <div class="contact-item">
           <div class="contact-icon">◈</div>
           <div class="contact-label">Ubicación</div>
-          <div class="contact-value">Medellín, Colombia</div>
+          <div class="contact-value">Colombia</div>
         </div>
         <div class="contact-item">
           <div class="contact-icon">◉</div>
